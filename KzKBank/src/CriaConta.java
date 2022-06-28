fae1c5ca3bebef064e30aead7d9603cbb423ea82
Iury Kozielski ferreira kozielski <meidiaenove@gmail.com>
2022-06-28 00:03:09 -0300

    Adicionando novas classes

---
+++ KzKBank/src/CriaConta.java
@@ -0,0 +1,18 @@
+
+public class CriaConta {
+
+		public static void main(String[] args) {
+			Conta primeiraConta = new Conta();
+			primeiraConta.saldo = 200;
+			System.out.println(primeiraConta.saldo);
+			
+			
+			Conta segundaConta = new Conta();
+			segundaConta.saldo = 50;
+			System.out.println(segundaConta.saldo);
+			
+			primeiraConta.saldo = segundaConta.saldo;
+			
+			System.out.println(primeiraConta.saldo);
+		}
+}
---
+++ KzKBank/src/TestaMetodo.java
@@ -0,0 +1,25 @@
+public class TestaMetodo {
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		boolean conseguiuTransferir = contaDaMarcela.transfere(300, contaDoPaulo);
+		
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println(conseguiuTransferir);
+		
+		contaDoPaulo.titular = "paulo silveira";
+		System.out.println(contaDoPaulo.titular);
+	}
+}
